afd1d51800d9ad989cf2360f53dbc5c788c5c311
Yevgeniy Zotov <jenya12345zotov@gmail.com>
2025-05-18 23:38:26 +0500

    refactor: упрощение сущности Offer под торговлю скинами

--- src/main/java/kz/project1/trade/dto/CreateOfferRequest.java
+++ src/main/java/kz/project1/trade/dto/CreateOfferRequest.java
@@ -4,8 +4,6 @@ import lombok.Data;
 
 @Data
 public class CreateOfferRequest {
-    private String title;
-    private String description;
     private Double price;
     private Long userId;
     private CreateItemRequest item;
